64513858505d78cd2b0d7725f7e55b0da67f3d4b
Ethan Robbins <ERobbins3@humana.com>
2020-10-26 14:24:01 -0400

    initial

---
+++ ProductShop/ProductShop/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductShop.Models
+{
+    public class Product
+    {
+
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Slug => Name.Replace(' ', '-');
+
+
+    }
+}
