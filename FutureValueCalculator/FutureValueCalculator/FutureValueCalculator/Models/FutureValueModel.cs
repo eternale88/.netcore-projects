c2ac9ef1cc63e0c375560ad1c1a3255932d980de
Ethan Robbins <ERobbins3@humana.com>
2020-11-13 16:58:12 -0500

    start futur value proj

---
+++ FutureValueCalculator/FutureValueCalculator/FutureValueCalculator/Models/FutureValueModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureValueCalculator.Models
+{
+    public class FutureValueModel
+    {
+        [Display(Name = "Monthly Investment:")]
+        public decimal MonthlyInvestment { get; set; }
+
+        [Display(Name = "Yearly Interest Rate:")]
+        public decimal YearlyInterestRate { get; set; }
+
+        [Display(Name = "Number of Years:")]
+        public int Years { get; set; }
+
+        public decimal Calculate()
+        {
+            int months = Years * 12;
+            decimal monthlyInterestRate = YearlyInterestRate / 12 / 100;
+
+            decimal futureValue = 0;
+
+          for(int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+
+            return futureValue;
+        }
+    }
+}
